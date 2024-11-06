a6f61f428f0b127470901d397e4dac4a9d1a3143
Ihor <zayatsihor1988@gmail.com>
2024-11-06 22:02:25 +0200

    every things works properly (need edit alerts)

--- php/index.php
+++ php/index.php
@@ -1,19 +1,26 @@
 <?php
-// Відображає інформацію про PHP
+
+// Debugging: output environment variable values
+echo 'MYSQL_SERVER_NAME: ' . getenv('MYSQL_SERVER_NAME') . '<br>';
+echo 'MYSQL_USER: ' . getenv('MYSQL_USER') . '<br>';
+echo 'MYSQL_PASSWORD: ' . getenv('MYSQL_PASSWORD') . '<br>';
+echo 'MYSQL_DATABASE: ' . getenv('MYSQL_DATABASE') . '<br>';
+
+// Display PHP information
 phpinfo();
 
-// Перевіряє з'єднання з MariaDB
-$servername = "mariadb";
-$username = "test_user";
-$password = "test_password";
-$dbname = "test_db";
+// Check the connection to MariaDB
+$servername = getenv('MYSQL_SERVER_NAME'); 
+$username = getenv('MYSQL_USER');
+$password = getenv('MYSQL_PASSWORD');
+$dbname = getenv('MYSQL_DATABASE');
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Перевіряємо з'єднання
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
+}
 echo "Connected successfully to database: " . $dbname;
 
 $conn->close();
